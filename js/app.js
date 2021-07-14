73345fc7e6fa4464702dcf23b3940eb4d56d2e57
tolik129 <boykoanatoliy@ukr.net>
2021-07-14 13:03:51 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// // Import jQuery module (npm i jquery)
+// import $ from 'jquery'
+// window.jQuery = $
+// window.$ = $
+
+// // Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+
